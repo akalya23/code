844af4517b5f11ba45e9aba8ab964686fddee9b2
akalya23 <akalyaammu26@gmail.com>
2018-08-06 14:46:33 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Even
+{
+int a=3;
+System.out.println(a);
+if(a%2==0)
+{
+System.out.println("number is odd");
+}
+else
+{
+System.out.println("number is even")
+}
